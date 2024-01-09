509fdbbed47a6d2835344e2cad11aa3f2c4f2e23
Rafał Majkowski <majkipl@gmail.com>
2024-01-09 17:32:11 +0100

    init project

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 chown -R www-data:www-data /var/www/html/public
+chown -R www-data:www-data /var/www/html/storage
 chmod -R 777 /var/www/html/public
+chmod -R 777 /var/www/html/storage
 
 composer install --no-interaction --no-progress
 npm install -g npm
+
+php artisan key:generate            # generujemy klucz
+php artisan migrate                 # robimy migracje wraz
+php artisan cache:clear             # czyścimy cache
+php artisan config:clear            # czyścimy konfiguracje
+php artisan route:clear             # czyścimy routing
+
+service cron start
 
 exec docker-php-entrypoint apache2-foreground
